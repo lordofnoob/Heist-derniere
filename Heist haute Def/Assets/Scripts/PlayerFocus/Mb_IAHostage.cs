04955ea360bfd727f252cf71a9a30828e031b7a5
Quentin Denois <Quentin.denois@hotmail.fr>
2019-11-07 17:35:03 +0100

    Pathfinding dynamique

--- Heist haute Def/Assets/Scripts/PlayerFocus/Mb_IAHostage.cs
+++ Heist haute Def/Assets/Scripts/PlayerFocus/Mb_IAHostage.cs
@@ -13,13 +13,16 @@ public enum HostageState
 
 public class Mb_IAHostage : Mb_Trial
 {
+    [Header("Stress Infos")]
     public float stress = 0f; //Percentage
     public Image stressBar;
 
-    public NavMeshAgent agent;
     public float trialsAreaSize = 5f;
 
+    [Header("Hostage State")]
     public HostageState state = HostageState.Free;
+
+    [Header("Hostage target")]
     public Mb_Player target;
 
     // Start is called before the first frame update
@@ -38,7 +41,7 @@ public class Mb_IAHostage : Mb_Trial
 
     public void RandomMovement()
     {
-        agent.SetDestination(transform.position + new Vector3(Random.Range(1, trialsAreaSize), 0f, Random.Range(1, trialsAreaSize)));
+        //agent.SetDestination(transform.position + new Vector3(Random.Range(1, trialsAreaSize), 0f, Random.Range(1, trialsAreaSize)));
     }
 
     public override void DoThings()
@@ -53,7 +56,7 @@ public class Mb_IAHostage : Mb_Trial
         //Debug.Log("FOLLOWING");
 
         Vector3 targetPos = target.transform.position;
-        agent.SetDestination(targetPos);
-        agent.stoppingDistance = 2f;
+        //agent.SetDestination(targetPos);
+        //agent.stoppingDistance = 2f;
     }
 }
